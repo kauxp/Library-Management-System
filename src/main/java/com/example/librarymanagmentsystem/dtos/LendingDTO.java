e117077a996970eb8a154d6f8779094fd26a4ff6
Paramjeet kaur <paramjeetkaur4326@gmail.com>
2024-05-02 00:46:09 +0530

    added Lending DTO

--- src/main/java/com/example/librarymanagmentsystem/dtos/LendingDTO.java
+++ src/main/java/com/example/librarymanagmentsystem/dtos/LendingDTO.java
@@ -1,17 +1,17 @@
 package com.example.librarymanagmentsystem.dtos;
 
 import jakarta.persistence.ManyToOne;
-import com.example.librarymanagmentsystem.models.bookModel.Book;
-import com.example.librarymanagmentsystem.models.patronModel.Patron;
+import com.example.librarymanagmentsystem.models.bookModel.*;
+import com.example.librarymanagmentsystem.models.patronModel.*;
 import com.example.librarymanagmentsystem.Models.lendingModel.LendingStatus;
 import java.util.Date;
 
 public class LendingDTO {
     private Long lendingID;
     @ManyToOne
-    private Book bookID;
+    private com.example.librarymanagmentsystem.Models.bookModel.Book bookID;
     @ManyToOne
-    private Patron patronID;
+    private com.example.librarymanagmentsystem.Models.patronModel.Patron patronID;
     private Date dateBorrowed;
     private Date dueDate;
     private Date dateReturned;
